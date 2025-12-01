09bb37d136228c0b800849ad627b799a3d081f22
Steffen Trumtrar <steffen@trumtrar.info>
2025-12-01 08:29:38 +0100

    hw: update initial compilable version
    Signed-off-by: Steffen Trumtrar <steffen@trumtrar.info>

--- hw/build.py
+++ hw/build.py
@@ -4,34 +4,46 @@
 #
 
 from migen import *
+
+from litex.gen import *
+
+from litex_boards.platforms import colorlight_i5
+
+from litex.soc.cores.clock import *
 from litex.soc.integration.soc_core import SoCCore
 from litex.soc.integration.builder import Builder
 
-from litex_boards.platforms import colorlight_i5
+from litex.soc.interconnect.csr import *
+
 from litex_boards.targets  import colorlight_i5 as colorlight_target
 
 class IceGuardSoC(SoCCore):
     def __init__(self, **kwargs):
-        platform = colorlight_i5.Platform()
+        platform = colorlight_i5.Platform(board="i5", toolchain="trellis")
+
+        #FIXME
+        self.crg = colorlight_target._CRG(platform, 50e6)
 
         # CPU / SoC Core setup
         SoCCore.__init__(
             self,
             platform     = platform,
             clk_freq     = int(50e6),
-            cpu_type     = "vexriscv",
-            integrated_rom_size  = 0x8000,
-            integrated_sram_size = 0x4000,
             **kwargs
         )
 
         # TODO: UART, Timer, LEDs etc. hinzufügen
 
 def main():
-    soc     = IceGuardSoC()
-    builder = Builder(soc, output_dir="build", csr_csv="csr.csv")
+    from litex.build.parser import LiteXArgumentParser
+    parser = LiteXArgumentParser(platform=colorlight_i5.Platform, description="IceGuard SoC on Colorlight i5")
+    args = parser.parse_args()
 
-    builder.build(run=False)
+    soc     = IceGuardSoC(**parser.soc_argdict)
+
+    builder = Builder(soc, **parser.builder_argdict)
+    if args.build:
+        builder.build(**parser.toolchain_argdict)
 
 if __name__ == "__main__":
     main()
