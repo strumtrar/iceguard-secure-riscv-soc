4418156dc38cd008cd9c3e19889db2c358803300
Steffen Trumtrar <s.trumtrar@pengutronix.de>
2025-12-17 18:09:55 +0100

    hw: add led chaser as first test
    Signed-off-by: Steffen Trumtrar <s.trumtrar@pengutronix.de>

--- hw/build.py
+++ hw/build.py
@@ -13,12 +13,14 @@ from litex.soc.cores.clock import *
 from litex.soc.integration.soc_core import SoCCore
 from litex.soc.integration.builder import Builder
 
+from litex.soc.cores.led import LedChaser
+
 from litex.soc.interconnect.csr import *
 
 from litex_boards.targets  import colorlight_i5 as colorlight_target
 
 class IceGuardSoC(SoCCore):
-    def __init__(self, **kwargs):
+    def __init__(self, sys_clk_freq=60e6, **kwargs):
         platform = colorlight_i5.Platform(board="i5", toolchain="trellis")
 
         #FIXME
@@ -28,18 +30,23 @@ class IceGuardSoC(SoCCore):
         SoCCore.__init__(
             self,
             platform     = platform,
-            clk_freq     = int(50e6),
+            clk_freq     = int(sys_clk_freq),
             **kwargs
         )
 
         # TODO: UART, Timer, LEDs etc. hinzufügen
+        ledn = platform.request_all("user_led_n")
+        self.leds = LedChaser(pads=ledn, sys_clk_freq=sys_clk_freq)
 
 def main():
     from litex.build.parser import LiteXArgumentParser
     parser = LiteXArgumentParser(platform=colorlight_i5.Platform, description="IceGuard SoC on Colorlight i5")
+    parser.add_target_argument("--sys-clk-freq",     default=60e6, type=float, help="System clock frequency.")
     args = parser.parse_args()
 
-    soc     = IceGuardSoC(**parser.soc_argdict)
+    soc     = IceGuardSoC(
+        sys_clk_freq           = args.sys_clk_freq,
+        **parser.soc_argdict)
 
     builder = Builder(soc, **parser.builder_argdict)
     if args.build:
